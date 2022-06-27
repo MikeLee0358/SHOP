082170bc0dc2a8a981d7af4f496d601f3284579e
MichaeLee1 <cspkno005@gmail.com>
2022-06-28 01:58:43 +0800

    origin

---
+++ src/main.js
@@ -0,0 +1,153 @@
+const main = document.querySelector('main')
+// stepper
+const stepOne = document.querySelector('.step')
+const stepTwo = document.querySelector('.step-two')
+const stepThree = document.querySelector('.step-three')
+
+// form
+const basicInfo = document.querySelector('.basic-info')
+const deliveryInfo = document.querySelector('.delivery-info')
+const cardInfo = document.querySelector('.card-info')
+
+// btn
+const btnGroup = document.querySelector('.btn-group')
+const nextBtn = document.querySelector('.next')
+const preBtn = document.querySelector('.previous')
+
+//order
+const orderPanel = document.querySelector('.order-panel')
+const deliveryFee = document.querySelector('.delivery-fee')
+const total = document.querySelector('.total')
+let numTop = document.querySelector('.num.top')
+let numBottom = document.querySelector('.num.bottom')
+
+let shoppingMoneyBox = 0
+let defaultMoney = 5298
+
+
+const STEPPER_STATE = {
+  StepStart: 1,
+  StepOne: 1,
+  StepTwo: 2,
+  StepThree: 3,
+}
+const controller = {
+  currentStep: STEPPER_STATE.StepStart
+}
+
+
+
+function changStepperState(event) {
+  const target = event.target
+
+  if (target.matches('.next')) {
+    controller.currentStep++
+    if (controller.currentStep > 3) return
+  }
+  if (target.matches('.previous')) {
+    controller.currentStep--
+    if (controller.currentStep < 1) return
+  }
+
+  nextBtn.innerHTML = '下一步'
+
+  switch (controller.currentStep) {
+    case STEPPER_STATE.StepOne:
+      stepTwo.classList.remove('active')
+      stepTwo.classList.remove('checked')
+
+      basicInfo.classList.remove('d-none')
+      deliveryInfo.classList.add('d-none')
+      cardInfo.classList.add('d-none')
+
+      preBtn.classList.add('d-none')
+      nextBtn.classList.add('w-f')
+      break;
+
+    case STEPPER_STATE.StepTwo:
+      stepOne.classList.remove('active')
+      stepOne.classList.add('checked')
+      stepTwo.classList.add('active')
+      stepThree.classList.remove('active')
+
+      basicInfo.classList.add('d-none')
+      deliveryInfo.classList.remove('d-none')
+      cardInfo.classList.add('d-none')
+
+      preBtn.classList.remove('d-none')
+      preBtn.classList.add('w-4')
+      nextBtn.classList.remove('w-f')
+      nextBtn.classList.add('w-4')
+      break;
+
+    case STEPPER_STATE.StepThree:
+      stepTwo.classList.remove('active')
+      stepTwo.classList.add('checked')
+      stepThree.classList.add('active')
+
+      basicInfo.classList.add('d-none')
+      deliveryInfo.classList.add('d-none')
+      cardInfo.classList.remove('d-none')
+
+      nextBtn.innerHTML = '確認下單'
+      break;
+  }
+}
+
+function pureNumber(domStr) {
+  // 過濾字元
+  let pattern = /[$,免費]/g
+  return Number(domStr.textContent.replace(pattern, ''))
+}
+
+
+function deliveryFeeChange(event) {
+  if (event.target.matches('.free')) {
+    deliveryFee.innerHTML = '免費'
+  } else {
+    deliveryFee.innerHTML = `$500`
+  }
+
+}
+
+orderPanel.addEventListener('click', function clickedOnOrderPanel(event) {
+  const target = event.target
+  const price = event.target.parentElement.nextElementSibling
+
+  // 上半部的購物欄
+  if (target.matches('.top')) {
+    if (target.matches('.plus')) {
+      target.previousElementSibling.textContent = pureNumber(numTop) + 1
+      shoppingMoneyBox = shoppingMoneyBox + pureNumber(price)
+    } else if (target.matches('.minus')) {
+      if (pureNumber(numTop) === 0) return
+      target.nextElementSibling.textContent = pureNumber(numTop) - 1
+      shoppingMoneyBox = shoppingMoneyBox - pureNumber(price)
+    }
+  }
+  // 下半部購物欄
+  if (target.matches('.bottom')) {
+    if (target.matches('.plus')) {
+      target.previousElementSibling.textContent = pureNumber(numBottom) + 1
+      shoppingMoneyBox = shoppingMoneyBox + pureNumber(price)
+    } else if (target.matches('.minus')) {
+      if (pureNumber(numBottom) === 0) return
+      target.nextElementSibling.textContent = pureNumber(numBottom) - 1
+      shoppingMoneyBox = shoppingMoneyBox - pureNumber(price)
+    }
+  }
+
+  total.textContent = shoppingMoneyBox + pureNumber(deliveryFee) + defaultMoney
+})
+
+
+
+
+btnGroup.addEventListener('click', function ClickOnBtnGroup(event) {
+  changStepperState(event)
+})
+deliveryInfo.addEventListener('click', function clickedOnDeliveryInfo(event) {
+  if (event.target.matches('LABEL')) {
+    deliveryFeeChange(event)
+  }
+})
